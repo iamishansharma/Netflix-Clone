ee8fb514070600c70bb2dbaa0a4a9aec6590edca
Ishan Sharma <sharmaishan747@gmail.com>
2021-08-16 15:53:47 +0530

    Firebase Setup Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
+import firebase from "./firebase";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAet_TbIqtppljJhmkRAF_pyBwX52zGCbY",
+  authDomain: "netflix-clone-ea077.firebaseapp.com",
+  projectId: "netflix-clone-ea077",
+  storageBucket: "netflix-clone-ea077.appspot.com",
+  messagingSenderId: "292958215949",
+  appId: "1:292958215949:web:1218b761e419382eaa9a44",
+  measurementId: "G-XWE183RYCZ",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
